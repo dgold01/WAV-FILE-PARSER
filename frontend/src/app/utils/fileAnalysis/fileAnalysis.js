1194031e13e4e9fa24866ff9ddd767851244701d
dgold01 <d.l.gold@hotmail.com>
2023-07-02 12:53:02 +0100

    added comments and changed mock data

--- frontend/src/app/utils/fileAnalysis/fileAnalysis.js
+++ frontend/src/app/utils/fileAnalysis/fileAnalysis.js
@@ -1,49 +1,63 @@
+// The FileReader is abstracted out of this function to allow for testing. In fileAnalysis.test.js, a mock FileReader is created, 
+// since we don't have access to the FileReader when not in browser.
 
-//the FileReader is abstaracted out of this function to allow for testting. In fileAnalysis.test.js, a mock FileReader is created, since we don't have access to the FileReader when not in browser.
+// This function takes a file reader object as the first argument and an optional second argument file object, the second argument is not used in testing.
+// The function returns a promise that will resolve with the WAV header info. If no WAV file is deteced, a error is thrown.
 
-export function extractWavInfo(reader,file) {
+export function extractWavInfo(reader, file) {
     return new Promise((resolve, reject) => {
-      reader.onload = function () {
-        const buffer = reader.result;
-        const view = new DataView(buffer);
-  
-        // checks to see if file is in WAV format. If not, promise is rejected
-        if (view.getUint32(0, true) !== 0x52494646 && view.getUint32(0, false) !== 0x52494646) {
-          reject(new Error('Not WAV file format'));
-        }
-  
-        // Extracts the sample rate and number of channels
-        const sampleRate = view.getUint32(24, true);
-        const channels = view.getUint16(22, true);
-        const chunkSize = view.getUint32(4, true);
-        const audioFormat = view.getUint16(20, true);
-        const byteRate = view.getUint32(28, true);
-        const blockAlign = view.getUint16(32, true);
-        const bitsPerSample = view.getUint16(34, true);
-        const subChunkID = view.getUint32(12, false);
-        const format = view.getUint32(8, false);
-        const chunkID = view.getUint32(0, false);
-  
-        resolve({
-          sampleRate: sampleRate,
-          channels: channels,
-          chunkSize: chunkSize,
-          audioFormat: audioFormat,
-          byteRate: byteRate,
-          blockAlign: blockAlign,
-          bitsPerSample: bitsPerSample,
-          subChunkID: subChunkID,
-          chunkID: chunkID,
-          format: format
-        });
-      };
-  
-      reader.onerror = function() {
-        reject(new Error('Error reading the file'));
-      };
+        // Event handler when the FileReader finishes loading the file
+        reader.onload = function () {
+            // Obtains the binary data from the FileReader's result
+            const buffer = reader.result;
 
-      if(file) reader.readAsArrayBuffer(file);
-      else{reader.readAsArrayBuffer(reader.file); }
+            // Creates a DataView object to interpret the binary data stored in the buffer.
+            const view = new DataView(buffer);
+
+
+            // Checks to see if file is in WAV format. If not, promise is rejected.
+            if (view.getUint32(0, true) !== 0x52494646 && view.getUint32(0, false) !== 0x52494646) {
+                reject(new Error('Not WAV file format'));
+            }
+
+            // Interprets the binary data as an integer and accesses the numerical information encoded within the data.
+            // First argument refers to the byte offset in the WAV header.
+
+            // These data chunks are in little-endian form, and so are extracted with the second argument 'true'
+            const sampleRate = view.getUint32(24, true);
+            const channels = view.getUint16(22, true);
+            const chunkSize = view.getUint32(4, true);
+            const audioFormat = view.getUint16(20, true);
+            const byteRate = view.getUint32(28, true);
+            const blockAlign = view.getUint16(32, true);
+            const bitsPerSample = view.getUint16(34, true);
+
+            // These data chunks are in big-endian form, and so are extracted with the second argument 'false'
+            const subChunkID = view.getUint32(12, false);
+            const format = view.getUint32(8, false);
+            const chunkID = view.getUint32(0, false);
+
+            resolve({
+                sampleRate: sampleRate,
+                channels: channels,
+                chunkSize: chunkSize,
+                audioFormat: audioFormat,
+                byteRate: byteRate,
+                blockAlign: blockAlign,
+                bitsPerSample: bitsPerSample,
+                subChunkID: subChunkID,
+                chunkID: chunkID,
+                format: format
+            });
+        };
+
+        // Event handler when FileReader encounters an error reading the file
+        reader.onerror = function () {
+            reject(new Error('Error reading the file'));
+        };
+        // Read the file as an array buffer using FileReader
+        if (file) reader.readAsArrayBuffer(file);
+        // If the file argument is not provided, assume that the FileReader itself has a file property
+        else { reader.readAsArrayBuffer(reader.file); }
     });
-  };
-  
+};
--- frontend/src/app/utils/fileAnalysis/fileAnalysis.test.js
+++ frontend/src/app/utils/fileAnalysis/fileAnalysis.test.js
@@ -1,9 +1,12 @@
-
 import { extractWavInfo } from './fileAnalysis';
-
+import { mockByteArrayWAV, mp4ByteArray } from '../mockData/mockBinaryData'
 
 describe('fileAnalysis function correctly parses WAV header', () => {
     test('it should return the correct information', async () => {
+
+        //Simulates a mock FileReader with inital result,onload and onerror properties set to null
+        //When this mock FileReader is passed as an argument into the extractWavInfo function, its onload property is assigmed to the onload function in extractWavInfo.
+        //Its readAsArrayBuffer is called, which sets the object's result peorpty to mockResult, and calls the onload function
         const mockReader = {
             result: null,
             onload: null,
@@ -16,30 +19,14 @@ describe('fileAnalysis function correctly parses WAV header', () => {
                 }
             },
         };
-        const byteArray = [0x52, 0x49, 0x46, 0x46, // 'RIFF' (chunkID)
-            0x24, 0x08, 0x00, 0x00, // Chunk size (replace with actual size)
-            0x57, 0x41, 0x56, 0x45, // 'WAVE' (format)
-            0x66, 0x6d, 0x74, 0x20, // 'fmt ' (subChunkID)
-            0x10, 0x00, 0x00, 0x00, // Subchunk size
-            0x01, 0x00, // Audio format
-            0x02, 0x00, // Number of channels
-            0x22, 0x56, 0x00, 0x00, // Sample rate
-            0x88, 0x58, 0x01, 0x00, // Byte rate
-            0x04, 0x00, // Block align
-            0x10, 0x00, // Bits per sample
-            0x64, 0x61, 0x74, 0x61, // 'data' (subChunkID)
-            0x00, 0x08, 0x00, 0x00, // Subchunk size (replace with actual size)
-            0x00, 0x00, 0x00, 0x00, // Waveform data (replace with actual data)
-            0x24, 0x17, 0x1e, 0xf3,
-            0x3c, 0x13, 0x3c, 0x14,
-            0x16, 0xf9, 0x18, 0xf9,
-            0x34, 0xe7, 0x23, 0xa6,
-            0x3c, 0xf2, 0x24, 0xf2,
-            0x11, 0xce, 0x1a, 0x0d,]
-        const mockResult = new Uint8Array(byteArray).buffer;
 
+        // Converts the mockByteArrayWAV to an ArrayBuffer representation, with the buffer property equal to the mockResult
+        const mockResult = new Uint8Array(mockByteArrayWAV).buffer;
+
+        //Calling the extractWavInfo function with the simlauted mockReader as the only argument
         const result = await extractWavInfo(mockReader);
 
+        // Expeted values are found in mockBinaryData.js
         expect(result).toEqual({
             sampleRate: 22050,
             channels: 2,
@@ -58,6 +45,9 @@ describe('fileAnalysis function correctly parses WAV header', () => {
 
     test('it should return an error if input file is not in .WAV format. ', async () => {
 
+        //Simulates a mock FileReader with inital result,onload and onerror properties set to null
+        //When this mock FileReader is passed as an argument into the extractWavInfo function, its onload property is assigmed to the onload function in extractWavInfo.
+        //Its readAsArrayBuffer is called, which sets the object's result peorpty to mockResult, and calls the onload function
         const mockReader = {
             result: null,
             onload: null,
@@ -70,22 +60,13 @@ describe('fileAnalysis function correctly parses WAV header', () => {
                 }
             },
         };
-        const mp4ByteArray = [
-            0x00, 0x00, 0x00, 0x18, // Box size
-            0x66, 0x74, 0x79, 0x70, // 'ftyp' box type
-            0x69, 0x73, 0x6F, 0x6D, // Major brand
-            0x00, 0x00, 0x00, 0x01, // Minor version
-            0x6D, 0x70, 0x34, 0x31, // Compatible brands (e.g., 'mp41')
-            0x00, 0x00, 0x00, 0x14, // Box size
-            0x6D, 0x6F, 0x6F, 0x76, // 'moov' box type
-            // ... Continue adding byte values for the rest of the MP4 file
-        ];
-
+        // Converts the mockByteArrayWAV to an ArrayBuffer representation, with the buffer property equal to the mockResult
         const mockResult = new Uint8Array(mp4ByteArray).buffer;
 
+        // A try-catch is used here to determine if the thrown error is the one expected
         try {
             const result = await extractWavInfo(mockReader); //this will throw an error
-        }catch(error){
+        } catch (error) {
             expect(error.message).toEqual('Not WAV file format'); //checks to see if correct error
         }
     })
